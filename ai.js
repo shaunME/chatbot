291bcfa72e0e1a4a31f249ad0bb069c14203b4bc
Don Nguyen <don@unswrc.com>
2017-07-05 21:46:38 +1000

    Minor update

--- ai.js
+++ ai.js
@@ -3,19 +3,18 @@
  	var response = "I'm sorry but I could not determine your salary. Please try again.";
  	var helpWords = ["help", "assist", "aid"];
 
- 	for (var i=0; i < helpWords.length; i++) {
- 		var helpWord = helpWords[i];
+ 	for (var helpWord of helpWords) {
  		if (userInput.toLowerCase().indexOf(helpWord) != -1) {
- 			return "It seems like you need assistance. Please visit http://www.test.com/help for further help";
- 			
+ 			return "It seems like you need assistance. Please visit http://www.test.com/help for further help";	
  		}
  	}
 
- 	var age = userInput.match(/[0-9]{1,3}/g);
+ 	var age = userInput.match(/\d+/g);
  	if (null == age) {
- 		return "I'm sorry. I could not determine your age";
+ 		return "I'm sorry. I could not determine your age. Please call (02) 1234 5678 for assistance.";
  	} else {
- 		if (parseInt(age[0]) > 125) {
+ 		var userAge = parseInt(age[0]);
+ 		if (userAge > 125) {
  			return "Your age is: " + age[0] + ". Are you sure?";
  		}
  		else {
